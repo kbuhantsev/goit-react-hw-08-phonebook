710e5eecc3889f0cb7035d9a0660116173a5b04e
Konstantyn <k.buhantsev@gmail.com>
2022-11-09 17:58:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <Provider store={store}>
-        <BrowserRouter basename="/goit-react-hw-07-phonebook">
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </Provider>
