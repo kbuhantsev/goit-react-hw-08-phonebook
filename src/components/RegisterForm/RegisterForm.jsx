deb4c02cbef3eee650aa351bbfd00c0c75000d7c
Konstantyn <k.buhantsev@gmail.com>
2022-11-10 17:30:53 +0200

    register form

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -3,17 +3,8 @@ import { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 import { ToastContainer, toast } from 'react-toastify';
-import {
-  Button,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  TextField,
-} from '@mui/material';
-import Box from 'components/Box';
-import { FormStyled } from './LoginForm.styled';
+import { Button, IconButton, InputAdornment, TextField } from '@mui/material';
+import { FormStyled } from './RegisterForm.styled';
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/user/operations';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
@@ -21,15 +12,20 @@ import { Visibility, VisibilityOff } from '@mui/icons-material';
 const emailRegExp = '';
 
 const schema = Yup.object({
+  name: Yup.string()
+    .min(2, 'Minimum 7 symbols!')
+    .max(30, 'Maximum 30 symbols!')
+    .required('This field is required!'),
   email: Yup.string()
     .matches(emailRegExp, 'Name is not valid!')
     .required('This field is required!'),
   password: Yup.string()
+    .min(7, 'Minimum 7 symbols!')
     .max(30, 'Maximum 30 symbols!')
     .required('This field is required!'),
 }).required();
 
-export default function RegisterForm() {
+export default function LoginForm() {
   const {
     control,
     handleSubmit,
@@ -37,6 +33,7 @@ export default function RegisterForm() {
     formState: { errors, isSubmitSuccessful },
   } = useForm({
     defaultValues: {
+      name: '',
       email: '',
       password: '',
     },
@@ -56,10 +53,6 @@ export default function RegisterForm() {
     }
   }, [isSubmitSuccessful, reset]);
 
-  const handleChange = prop => event => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
-
   const handleClickShowPassword = () => {
     setValues({
       ...values,
@@ -72,6 +65,7 @@ export default function RegisterForm() {
   };
 
   const onFormSubmit = data => {
+    console.log(data);
     dispatch(logIn(data));
   };
 
@@ -80,8 +74,24 @@ export default function RegisterForm() {
   };
 
   return (
-    <Box>
+    <>
       <FormStyled onSubmit={handleSubmit(onFormSubmit, onFormError)}>
+        <Controller
+          name="name"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Username"
+              variant="outlined"
+              size="small"
+              error={errors.name && true}
+              helperText={errors.name?.message}
+              required
+            />
+          )}
+        />
+
         <Controller
           name="email"
           control={control}
@@ -97,35 +107,39 @@ export default function RegisterForm() {
             />
           )}
         />
-        <FormControl>
-          <InputLabel htmlFor="password" size="small">
-            Password *
-          </InputLabel>
-          <OutlinedInput
-            id="password"
-            name="password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            label="Password"
-            size="small"
-            onChange={handleChange('password')}
-            error={errors.email && true}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-        </FormControl>
+
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Password"
+              variant="outlined"
+              size="small"
+              error={errors.password && true}
+              helperText={errors.password?.message}
+              type={values.showPassword ? 'text' : 'password'}
+              required
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+          )}
+        />
+
         <Button type="submit" variant="outlined">
-          Login
+          Register
         </Button>
       </FormStyled>
 
@@ -140,6 +154,6 @@ export default function RegisterForm() {
         pauseOnHover
         theme="colored"
       />
-    </Box>
+    </>
   );
 }
