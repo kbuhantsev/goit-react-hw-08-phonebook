f4ed3d41985971dda0f816518864aacef1178cc0
Konstantyn <k.buhantsev@gmail.com>
2022-11-10 18:07:04 +0200

    useUser

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,7 @@ import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 import Phones from '../pages/Phones';
 import Register from '../pages/Register';
+// import { useUser } from 'hooks';
 
 // const Home = lazy(() => import('./pages/Home'));
 // const NotFound = lazy(() => import('./pages/NotFound'));
@@ -16,6 +17,8 @@ import Register from '../pages/Register';
 // const Phones = lazy(() => './pages/Phones');
 
 export default function App() {
+  // const { isLoggedIn, user } = useUser();
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
--- src/components/AppBar/UserMenu/UserMenu.jsx
+++ src/components/AppBar/UserMenu/UserMenu.jsx
@@ -1,10 +1,27 @@
 import { Button } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import EmailIcon from '@mui/icons-material/Email';
 import Box from 'components/Box';
+import { useUser } from 'hooks';
 
 export default function UserMenu() {
+  const {
+    user: { name, email },
+  } = useUser();
+
   return (
-    <Box flexDirection="row" gridGap="20px">
-      <p style={{ alignSelf: 'center' }}>mango@mail.com</p>
+    <Box flexDirection="row" gridGap="20px" justifyContent="center">
+      <Box as="ul" flexDirection="row" gridGap="10px">
+        <Box as="ul" flexDirection="row" gridGap="5px">
+          <AccountCircleIcon color="primary" />
+          {name}
+        </Box>
+        <Box as="ul" flexDirection="row" gridGap="5px">
+          <EmailIcon color="primary" />
+          {email}
+        </Box>
+      </Box>
+
       <Button variant="contained">Logout</Button>
     </Box>
   );
