61a77fda3dd27961b66a3894cac08a7210e9665e
Konstantyn <k.buhantsev@gmail.com>
2023-02-15 11:59:33 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,9 @@
 import Box from 'components/Box';
 import { useTheme } from '@mui/material/styles';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
-import { useUser } from 'hooks';
-import { useLocation } from 'react-router-dom';
 
 export default function Home() {
   const theme = useTheme();
-  const { isVerifying } = useUser();
-
-  let location = useLocation();
-  let user = null;
-  if (location.state !== null && typeof location.state === 'object') {
-    user = location.state.user;
-  }
 
   return (
     <Box>
@@ -28,15 +19,6 @@ export default function Home() {
       >
         Phonebook home page
       </Box>
-      {isVerifying && user ? (
-        <div>
-          Dear {user.name}, we sent you email to confirm your account.
-          <br />
-          Check your email.
-        </div>
-      ) : (
-        <></>
-      )}
     </Box>
   );
 }
